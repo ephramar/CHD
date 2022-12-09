ac6970512d40731f8c5930573157c24fccd5c9b8
Ephramar <ephramar@users.noreply.github.com>
2022-12-09 11:20:32 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -39,7 +39,7 @@
     <?php include 'common/nav-side.php'; ?>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 mt-3">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 mt-3 border-bottom">
         <div class="container">
           <div class="row row-cols-1 row-cols-md-6 text-center">
             <div class="col">
@@ -112,7 +112,9 @@
         </div>
       </div>
 
-      <h2>List of Patients</h2>
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 mt-3">
+        <h1 class="h2">List of Patients</h1>
+      </div>
       <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
@@ -243,18 +245,8 @@
         </table>
       </div>
 
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 mt-3">
         <h1 class="h2">Volume of Patients</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-          </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            <span data-feather="calendar" class="align-text-bottom"></span>
-            This week
-          </button>
-        </div>
       </div>
 
       <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
